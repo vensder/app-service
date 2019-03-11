9a1b92eca842f33b42e5117567973607826f983b
Dmitrii <dmitrii@Dmitrii-Makarovs-MacBook-Pro.local>
2019-03-12 10:31:13 +1300

    added routes

--- hello.py
+++ hello.py
@@ -3,6 +3,22 @@
 from flask import Flask
 myapp = Flask(__name__)
 
-@myapp.route("/")
-def hello():
-    return "Hello Flask, on Azure App Service for Linux"
+# @myapp.route("/")
+# def hello():
+#     return "Hello Flask, on Azure App Service for Linux"
+
+@myapp.route('/', defaults={'path': ''})
+@myapp.route('/<path:path>')
+def hello(path):
+    return '<b>Welcome to %s admin page</b>\
+    </br>\
+			You need the authorization to access this page\
+		</br>\
+		</br>\
+		<form>\
+	  		User name:<br>\
+	  			<input type="text" name="username"><br>\
+	  		User password:<br>\
+	  			<input type="password" name="psw">\
+		</form>\
+  			<input type="submit" value="Submit">' % path
